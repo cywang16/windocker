fa308d93b73d55e4b888e0088d6e96d81a9157d1
Chun-Yen Wang <cywang16@gmail.com>
2020-03-29 23:06:31 -0700

    NorthwindDbApp skeleton

---
+++ NorthwindDbApp/NorthwindDbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NorthwindDbApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
